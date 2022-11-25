900de55c24e1f8cf2c5ac69eb6a6e1e2814d6f43
DawidKrupski <dawid.krupski323@gmail.com>
2022-11-25 09:28:15 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -14,7 +14,7 @@ const saveInformations = event => {
 
 form.addEventListener('keyup', throttle(saveInformations, 500));
 
-if (localStorage.length !== null) {
+if (localStorage.length !== 0) {
   const loadValue = localStorage.getItem('feedback-form-state');
   email.value = JSON.parse(loadValue)[0];
   message.value = JSON.parse(loadValue)[1];
